2d129b314c3daa61c823153c1c1a2cdbcc6b82eb
Jonatas <modesto.jonatas18@gmail.com>
2020-01-28 18:50:18 -0300

    First commit

---
+++ Vetor devolve o maior/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vetor_devolve_o_maior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[7];
+            int maior = 0;
+
+            for(int i = 0; i < 7; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                v[i] = x;
+                if (maior < x)
+                {
+                    maior = x;
+                }
+            }
+            Console.WriteLine(maior);
+        }
+    }
+}
